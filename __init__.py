51121020816d3cda5e165c438663c62cc69043d2
glowies <comuokta@gmail.com>
2025-10-15 08:18:49 -0400

    fix: use pylsp and ruff separately
    Signed-off-by: glowies <comuokta@gmail.com>

--- __init__.py
+++ __init__.py
@@ -5,46 +5,42 @@ import bpy
 #     # module1,
 # )
 
-bl_info = {
-    "name": "Darkroom Blend",
-    "author": "Oktay Comu",
-    "description": "An addon to develop linear EXR images in Blender.",
-    "blender": (4, 0, 0),
-    "version": (0, 0, 1),
-    "location": "",
-    "warning": "",
-    "category": "Generic",
-}
-
 if _needs_reload:
-    import importlib
+    # import importlib
     # module3 = importlib.reload(module3)
-    print(f"Add-on Reloaded: {bl_info['name']}")
+    print("Add-on Reloaded")
 
 
 class ObjectMoveX(bpy.types.Operator):
-    """My Object Moving Script"""      # Use this as a tooltip for menu items and buttons.
-    bl_idname = "object.move_x"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Move X by One"         # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+    """My Object Moving Script"""  # Use this as a tooltip for menu items and buttons.
 
-    def execute(self, context):        # execute() is called when running the operator.
+    bl_idname = (
+        "object.move_x"  # Unique identifier for buttons and menu items to reference.
+    )
+    bl_label = "Move X by One"  # Display name in the interface.
+    bl_options = {"REGISTER", "UNDO"}  # Enable undo for the operator.
 
+    def execute(self, context):  # execute() is called when running the operator.
         # The original script
         scene = context.scene
         for obj in scene.objects:
             obj.location.x += 1.0
 
-        self.report({'INFO'}, "Running Move X by One")
+        self.report({"INFO"}, "Running Move X by One")
 
-        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+        return {"FINISHED"}  # Lets Blender know the operator finished successfully.
+
 
 def menu_func(self, context):
     self.layout.operator(ObjectMoveX.bl_idname)
 
+
 def register():
     bpy.utils.register_class(ObjectMoveX)
-    bpy.types.VIEW3D_MT_object.append(menu_func)  # Adds the new operator to an existing menu.
+    bpy.types.VIEW3D_MT_object.append(
+        menu_func
+    )  # Adds the new operator to an existing menu.
+
 
 def unregister():
     bpy.utils.unregister_class(ObjectMoveX)
