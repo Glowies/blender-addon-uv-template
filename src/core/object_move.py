b6ab248206bc398c8f2aecf414d5e447a99dbcf7
Oktay Comu <self@oktaycomu.com>
2025-10-20 20:27:36 -0400

    feat: auto reload of classes
    Signed-off-by: Oktay Comu <self@oktaycomu.com>

--- src/core/object_move.py
+++ src/core/object_move.py
@@ -1,14 +1,4 @@
-_needs_reload = "bpy" in locals()
-
 import bpy
-# from . import (
-#     # module1,
-# )
-
-if _needs_reload:
-    # import importlib
-    # module3 = importlib.reload(module3)
-    print("Add-on Reloaded")
 
 
 class ObjectMoveX(bpy.types.Operator):
@@ -17,14 +7,14 @@ class ObjectMoveX(bpy.types.Operator):
     bl_idname = (
         "object.move_x"  # Unique identifier for buttons and menu items to reference.
     )
-    bl_label = "Move X by One"  # Display name in the interface.
+    bl_label = "Move X by something"  # Display name in the interface.
     bl_options = {"REGISTER", "UNDO"}  # Enable undo for the operator.
 
     def execute(self, context):  # execute() is called when running the operator.
         # The original script
         scene = context.scene
         for obj in scene.objects:
-            obj.location.x += 1.0
+            obj.location.x += 10.0
 
         self.report({"INFO"}, "Running Move X by One")
 
@@ -33,14 +23,3 @@ class ObjectMoveX(bpy.types.Operator):
 
 def menu_func(self, context):
     self.layout.operator(ObjectMoveX.bl_idname)
-
-
-def register():
-    bpy.utils.register_class(ObjectMoveX)
-    bpy.types.VIEW3D_MT_object.append(
-        menu_func
-    )  # Adds the new operator to an existing menu.
-
-
-def unregister():
-    bpy.utils.unregister_class(ObjectMoveX)
